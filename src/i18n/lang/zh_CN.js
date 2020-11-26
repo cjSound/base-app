6db80ae90c2d016dc34a47cea9077d69f3c7a621
cjSound <876304802@qq.com>
2020-11-26 10:47:04 +0800

    1

--- src/i18n/lang/zh_CN.js
+++ src/i18n/lang/zh_CN.js
@@ -7,23 +7,7 @@
  */
 module.exports = {
   agent: {
-    placeEmail: '请输入邮箱地址',
-    btnNext: '下一步',
-    emailFormatError: '请输入正确的邮箱',
-    emailExist: '此邮箱已被招待',
-    contactUs: '联系我们',
-    officialAccount: '官方账号',
-    storeName: '店铺名',
-    placeAgent: '请输入招待码',
-    agentName: '招待者名',
-    submit: '提 交',
-    toHome: '返回首页',
-    submitSuccess: '提交成功'
   },
   code: {
-    '1052': '此招待码已失效',
-    '1046': '代理商不存在',
-    '1002': '此邮箱为非注册邮箱',
-    '1045': '此邮箱已被招待'
   }
 }
