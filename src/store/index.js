6fd6a295d1146a040fe4a208ebaf053f52ec84a6
cjSound <876304802@qq.com>
2021-01-05 10:04:34 +0800

    vuex持久化

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,32 @@
  * @Author: 曹捷
  * @Date: 2019-08-01 14:25:13
  * @LastEditors: 曹捷
- * @LastEditTime: 2020-08-06 15:02:09
+ * @LastEditTime: 2021-01-05 10:02:49
  * @Description: store  集成common  modules和 modules
  */
 import Vue from 'vue'
 import Vuex from 'vuex'
+// vuex本地持久化插件
+import createPersistedState from 'vuex-persistedstate'
+
+const vuexPersisted = new createPersistedState({
+  storage: {
+    getItem: key => {
+      let item = uni.getStorageSync(key)
+      return item
+    },
+    setItem: (key, value) => {
+      return uni.setStorageSync(key, value)
+    },
+    removeItem: key => uni.removeStorageSync(key)
+  },
+  // reducer (val) {
+  //   return {
+  //     // 只储存state中的user
+  //     menu: val.menu
+  //   }
+  // }
+})
 Vue.use(Vuex)
 const commonFiles = require.context('@/app-modules/store', true, /\.js$/)
 const commons = commonFiles.keys().reduce((modules, modulePath) => {
@@ -24,6 +45,9 @@ const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   return modules
 }, {})
 
-const store = new Vuex.Store({ modules: Object.assign(commons, modules) })
+const store = new Vuex.Store({
+  modules: Object.assign(commons, modules),
+  plugins: [vuexPersisted]
+})
 
 export default store
